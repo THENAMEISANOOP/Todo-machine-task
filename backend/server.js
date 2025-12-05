74b451ae7f800deeb736f79cbb2c5daf1636bf81
THENAMEISANOOP <sanoopanoop444@gmail.com>
2025-12-05 13:06:37 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import taskRoutes from "./routes/taskRoutes.js";
+import router from "./routes/taskRoutes.js";
 import { connectDB } from "./db.js";
 
 dotenv.config();
-const app = express();
 
-// Middleware
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api", taskRoutes);
+app.use("/api", router);
 
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
-connectDB()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => {
-    console.error("failed to connect to mongoDB", err);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
+});
